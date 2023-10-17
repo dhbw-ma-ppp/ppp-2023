fce4502b34873416b3d1dd1866704bef1f3ee253
GeFritz <gfritz.business@gmail.com>
2023-10-17 13:15:05 +0200

    added more edge cases because why not

--- Exercises/GerritFritz/exercises_02.py
+++ Exercises/GerritFritz/exercises_02.py
@@ -33,7 +33,7 @@ for i in range(3):
 
 testArgs1 = ("test", "hi", "a","-1","123", "hey456")
 testArgs2 = ("a","bc","123325g3","1,2","1","4")
-testArgs3 = ("a1233","b","123325g3","-0","--2","99..9999")
+testArgs3 = ("a1233","b","123325g3","1-0","-2","99..9999")
 
 def getArgLists(*args):
     numberList = []
@@ -43,6 +43,8 @@ def getArgLists(*args):
             singleList.append(element)
         num = str("".join([char for char in element if char in "-.1234567890"]))
         if num == element and element.count(".")<=1 and element.count("-")<=1:
+            if "-" in element and element.index("-")>=1:
+                continue
             numberList.append(element)
     return numberList,singleList
 
