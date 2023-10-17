9c6d5410257852f1ad91dbff098a34ff4d9db02c
GeFritz <gfritz.business@gmail.com>
2023-10-17 13:02:26 +0200

    added point and negative support

--- Exercises/GerritFritz/exercises_02.py
+++ Exercises/GerritFritz/exercises_02.py
@@ -31,9 +31,9 @@ for i in range(3):
 
 # Aufgabe 2
 
-testArgs1 = ("test", "hi", "a","1","123", "hey456")
-testArgs2 = ("a","bc","123325g3","12","1","4")
-testArgs3 = ("a1233","b","123325g3","0","2","999999")
+testArgs1 = ("test", "hi", "a","-1","123", "hey456")
+testArgs2 = ("a","bc","123325g3","1,2","1","4")
+testArgs3 = ("a1233","b","123325g3","-0","2","99.9999")
 
 def getArgLists(*args):
     numberList = []
@@ -41,7 +41,7 @@ def getArgLists(*args):
     for element in args:
         if len(element)==1:
             singleList.append(element)
-        num = str("".join([char for char in element if char in "1234567890"]))
+        num = str("".join([char for char in element if char in "-.1234567890"]))
         if num == element:
             numberList.append(element)
     return numberList,singleList
