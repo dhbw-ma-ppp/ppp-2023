66d0ae01134918fb989ec499a43c5a04b0df3640
LeMixer <78697721+LeMixer@users.noreply.github.com>
2023-10-20 17:10:58 +0200

    task 1 initial commit

---
+++ Exercises/RobertWalz/exercises_03.py
@@ -0,0 +1,80 @@
+# PART 1:
+# Write a class for a French deck of cards (2-Ace of diamonds, hearts, spades, clubs).
+# The deck of cards should behave like a sequence.
+# When initialized the cards should all be in a well-defined order (2-Ace of each suite, suites in the order above
+# I should be able to index into the deck to retrieve one card.
+# I should be able to iterate over all cards in the deck.
+# Printing a cards string representation should give me a nice,
+# readable description of that card.
+
+
+from collections import UserList
+
+
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def __str__(self):
+        return f"{self.value} of {self.suit}s"
+
+
+class FrenchDeck(UserList):
+    _suits = ["Diamond", "Heart", "Spade", "Club"]
+    _values = [
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "Jack",
+        "Queen",
+        "King",
+        "Ace",
+    ]
+
+    def __init__(self):
+        self.data = []
+        for suit in self.__class__._suits:
+            for value in self.__class__._values:
+                self.data.append(Card(suit, value))
+
+
+class SkatDeck(FrenchDeck):
+    """A French deck without the cards 2-6"""
+    _values = FrenchDeck._values[5:]
+
+
+deck = SkatDeck()
+print(*(x for x in deck), sep="\n")
+
+
+# PART 2:
+# Create a second class that represents a deck of cards usable for Skat -- it should only contain cards from 7 upwards.
+# It should offer all the same functionality of the first class.
+
+
+# Write some code to test the functionality of both kinds of decks. (You can use `assert` to make sure your classes behave the way you expect them to.)
+
+
+# PART 3:
+# write a function that accepts two numbers, a lower bound and an upper bound.
+# the function should then return the count of all numbers that meet certain criteria:
+# - they are within the (left-inclusive and right-exclusive) bounds passed to the function
+# - there is at least one group of exactly two adjacent digits within the number which are the same (like 33 in 123345)
+# - digits only increase going from left to right
+#
+# Examples:
+# - 123345 is a valid number
+# - 123341 is not a valid number, as the digits do not increase from left to right
+# - 123334 is not a valid number as there is no group of exactly two repeated digits
+# - 111334 is a valid number. while there are three 1s, there is also a group of exactly two 3s.
+# - 112233 is a valid number. At least one group of two is fulfilled, there is no maximum to the number of such groups.
+#
+# run your function with the lower bound `134564` and the upper bound `585159`. Note the resulting count
+# in your pull request, please.
