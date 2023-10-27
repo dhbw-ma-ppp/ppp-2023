273777338b9360f80d3a0019d179a50f383cf77d
CorvinAn97 <135810521+CorvinAn97@users.noreply.github.com>
2023-10-27 09:04:31 +0200

    Have taken into account the changes suggested by Bo

--- Exercises/Corvin Ansorge/exercises_03.py
+++ Exercises/Corvin Ansorge/exercises_03.py
@@ -6,10 +6,32 @@
 # I should be able to iterate over all cards in the deck.
 # Printing a cards string representation should give me a nice, 
 # readable description of that card.
-import select
 
-#print("\nTask 1\n")
 
+# PART 2:
+# Create a second class that represents a deck of cards usable for Skat -- it should only contain cards from 7 upwards.
+# It should offer all the same functionality of the first class.
+
+
+# Write some code to test the functionality of both kinds of decks. (You can use `assert` to make sure your classes behave the way you expect them to.)
+
+
+# PART 3:
+# write a function that accepts two numbers, a lower bound and an upper bound.
+# the function should then return the count of all numbers that meet certain criteria:
+# - they are within the (left-inclusive and right-exclusive) bounds passed to the function
+# - there is at least one group of exactly two adjacent digits within the number which are the same (like 33 in 123345)
+# - digits only increase going from left to right
+#
+# Examples:
+# - 123345 is a valid number
+# - 123341 is not a valid number, as the digits do not increase from left to right
+# - 123334 is not a valid number as there is no group of exactly two repeated digits
+# - 111334 is a valid number. while there are three 1s, there is also a group of exactly two 3s.
+# - 112233 is a valid number. At least one group of two is fulfilled, there is no maximum to the number of such groups.
+#
+# run your function with the lower bound `134564` and the upper bound `585159`. Note the resulting count
+# in your pull request, please.
 class Card:
     def __init__(self, value, suit):
         self.value = value
@@ -44,96 +66,41 @@ class ScatDeck(FrenchDeck):
 frenchdeck = FrenchDeck()
 scatdeck = ScatDeck()
 
-
-# Write some code to test the functionality of both kinds of decks. (You can use `assert` to make sure your classes behave the way you expect them to.)
-
 def test_frenchdeck():
     assert len(frenchdeck.cards) == 52
-    assert frenchdeck.cards[0] == Card("2", "diamonds")
-    assert frenchdeck.cards[51] == Card("Ace", "clubs")
-    assert frenchdeck.cards[12] == Card("Ace", "diamonds")
-    assert frenchdeck.cards[13] == Card("2", "hearts")
-    assert frenchdeck.cards[25] == Card("Jack", "spades")
-    assert frenchdeck.cards[38] == Card("Queen", "clubs")
-    assert frenchdeck.cards[51] == Card("Ace", "clubs")
-
+    assert frenchdeck[0] == Card("2", "diamonds")
+    assert frenchdeck[51] == Card("Ace", "clubs")
+    assert frenchdeck[12] == Card("Ace", "diamonds")
+    assert frenchdeck[13] == Card("2", "hearts")
+    assert frenchdeck[25] == Card("Jack", "spades")
+    assert frenchdeck[38] == Card("Queen", "clubs")
+    assert frenchdeck[51] == Card("Ace", "clubs")
 
 def test_scatdeck():
     assert len(scatdeck.cards) == 32
-    assert scatdeck.cards[0] == Card("7", "diamonds")
-    assert scatdeck.cards[31] == Card("Ace", "clubs")
-    assert scatdeck.cards[12] == Card("Ace", "diamonds")
-    assert scatdeck.cards[13] == Card("7", "hearts")
-    assert scatdeck.cards[25] == Card("Jack", "spades")
-
-
-
+    assert scatdeck[0] == Card("7", "diamonds")
+    assert scatdeck[31] == Card("Ace", "clubs")
+    assert scatdeck[12] == Card("Ace", "diamonds")
+    assert scatdeck[13] == Card("7", "hearts")
+    assert scatdeck[25] == Card("Jack", "spades")
 
 #print(frenchdeck.cards)
 #print(scatdeck.cards)
 
+def check_number(number):
+    number = str(number)
+    if number != "".join(sorted(number)):
+        return False
+    for i in range(10):
+        if str(i)*2 in number and str(i)*3 not in number:
+            return True
+    return False
 
-
-
-
-# PART 2:
-# Create a second class that represents a deck of cards usable for Skat -- it should only contain cards from 7 upwards.
-# It should offer all the same functionality of the first class.
-
-
-# Write some code to test the functionality of both kinds of decks. (You can use `assert` to make sure your classes behave the way you expect them to.)
-
-
-
-
-
-#print("\nTask 2\n")
-
-
-
-
-
-
-
-
-
-# PART 3:
-# write a function that accepts two numbers, a lower bound and an upper bound.
-# the function should then return the count of all numbers that meet certain criteria:
-# - they are within the (left-inclusive and right-exclusive) bounds passed to the function
-# - there is at least one group of exactly two adjacent digits within the number which are the same (like 33 in 123345)
-# - digits only increase going from left to right
-#
-# Examples:
-# - 123345 is a valid number
-# - 123341 is not a valid number, as the digits do not increase from left to right
-# - 123334 is not a valid number as there is no group of exactly two repeated digits
-# - 111334 is a valid number. while there are three 1s, there is also a group of exactly two 3s.
-# - 112233 is a valid number. At least one group of two is fulfilled, there is no maximum to the number of such groups.
-#
-# run your function with the lower bound `134564` and the upper bound `585159`. Note the resulting count
-# in your pull request, please.
-
-#print("\nTask 3\n")
-
-def count_valid_numbers(lower_bound, upper_bound):
+def count_numbers(lower_bound, upper_bound):
     count = 0
-    for num in range(lower_bound, upper_bound):
-        num_str = str(num)
-        has_adjacent = False
-        increasing = True
-
-        for i in range(len(num_str) - 1):
-            if num_str[i] > num_str[i + 1]:
-                increasing = False
-                break
-            if num_str[i] == num_str[i + 1] and (i == 0 or num_str[i - 1] != num_str[i]) and (i + 2 == len(num_str) or num_str[i + 2] != num_str[i]):
-                has_adjacent = True
-
-        if increasing and has_adjacent:
+    for i in range(lower_bound, upper_bound):
+        if check_number(i):
             count += 1
-
     return count
 
-print(count_valid_numbers(134564, 585159))
-
+print(count_numbers(134564, 585159))
