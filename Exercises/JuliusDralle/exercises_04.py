0fbb875e975c2db0d49c68c9def4bf446f5da21e
RubelOS <julius.dralle@gmx.de>
2023-11-01 13:08:04 +0100

    Final solution

--- Exercises/JuliusDralle/exercises_04.py
+++ Exercises/JuliusDralle/exercises_04.py
@@ -52,57 +52,246 @@
 # Finally, run you code for the following instructions; when asked for input provide the number '5'. The program should print a single number when executed.
 # Please take note of that number in your PR, so I don't need to run all the files myself :)
 commands = [3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1101, 40, 71, 224, 1001, 224, -111, 224, 4, 224, 1002, 223, 8, 223, 101, 7, 224, 224, 1, 224, 223, 223, 1102, 66, 6, 225, 1102, 22, 54, 225, 1, 65, 35, 224, 1001, 224, -86, 224, 4, 224, 102, 8, 223, 223, 101, 6, 224, 224, 1, 224, 223, 223, 1102, 20, 80, 225, 101, 92, 148, 224, 101, -162, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 5, 224, 224, 1, 224, 223, 223, 1102, 63, 60, 225, 1101, 32, 48, 225, 2, 173, 95, 224, 1001, 224, -448, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 4, 224, 1, 224, 223, 223, 1001, 91, 16, 224, 101, -79, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 3, 224, 224, 1, 224, 223, 223, 1101, 13, 29, 225, 1101, 71, 70, 225, 1002, 39, 56, 224, 1001, 224, -1232, 224, 4, 224, 102, 8, 223, 223, 101, 4, 224, 224, 1, 223, 224, 223, 1101, 14, 59, 225, 102, 38, 143, 224, 1001, 224, -494, 224, 4, 224, 102, 8, 223, 223, 101, 3, 224, 224, 1, 224, 223, 223, 1102, 30, 28, 224, 1001, 224, -840, 224, 4, 224, 1002, 223, 8, 223, 101, 4, 224, 224, 1, 223, 224, 223, 4, 223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 107, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 329, 1001, 223, 1, 223, 8, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 344, 101, 1, 223, 223, 7, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 359, 101, 1, 223, 223, 1007, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 374, 1001, 223, 1, 223, 1007, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 389, 101, 1, 223, 223, 1008, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 404, 1001, 223, 1, 223, 108, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 419, 1001, 223, 1, 223, 1108, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 434, 1001, 223, 1, 223, 108, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 449, 101, 1, 223, 223, 7, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 464, 1001, 223, 1, 223, 8, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 479, 1001, 223, 1, 223, 107, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 494, 101, 1, 223, 223, 1007, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 509, 1001, 223, 1, 223, 1107, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 524, 1001, 223, 1, 223, 108, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 539, 101, 1, 223, 223, 1107, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 554, 1001, 223, 1, 223, 107, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 569, 101, 1, 223, 223, 8, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 584, 1001, 223, 1, 223, 7, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 599, 101, 1, 223, 223, 1008, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 614, 101, 1, 223, 223, 1008, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 629, 1001, 223, 1, 223, 1108, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 644, 101, 1, 223, 223, 1108, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 659, 1001, 223, 1, 223, 1107, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 674, 1001, 223, 1, 223, 4, 223, 99, 226]
+running = True
 
 
-def save_at(*args):
-    print("save_at ausgeführt", args)
-    return 0
+def reveal_parameters_in_opcode(opcode):
+    parameters_in_opcode = opcode // 100
+    if parameters_in_opcode < 10:
+         parameter_one = parameters_in_opcode
+         parameter_two = 0
+         parameter_three = 0
+    elif parameters_in_opcode < 100:
+         parameter_one = parameters_in_opcode % 10
+         parameter_two = parameters_in_opcode // 10
+         parameter_three = 0
+    elif parameters_in_opcode < 1000:
+         parameter_one = parameters_in_opcode % 10
+         parameter_two = parameters_in_opcode % 100 // 10
+         parameter_three = parameters_in_opcode // 100
+    return parameter_one, parameter_two, parameter_three
+
+def addition(opcode, index, commands):
+    parametermode_one, parametermode_two, parametermode_three = reveal_parameters_in_opcode(opcode)
+    num_arguments_needed = 3
+
+    if parametermode_one:
+         value_one = commands[index+1]
+    else:
+         value_one = int(commands[commands[index+1]])
+
+    if parametermode_two:
+         value_two = commands[index+2]
+    else:
+         value_two = int(commands[commands[index+2]])
+         
+    commands[commands[index+3]] = int(value_one + value_two)
+    return 0, num_arguments_needed
+
+def multiply(opcode, index, commands):
+    try:
+        parametermode_one, parametermode_two, parametermode_three = reveal_parameters_in_opcode(opcode)
+        num_arguments_needed = 3
+
+        if parametermode_one:
+            value_one = commands[index+1]
+        else:
+            value_one = int(commands[commands[index+1]])
+
+        if parametermode_two:
+            value_two = commands[index+2]
+        else:
+            value_two = int(commands[commands[index+2]])
+            
+        commands[commands[index+3]] = value_one * value_two
+    except KeyError:
+        print("Key Error at Index: ", index)
+    return 0, num_arguments_needed
+
+def save_at(opcode, index, commands):
+    num_arguments_needed = 1
+    parametermode_one, parametermode_two, parametermode_three = reveal_parameters_in_opcode(opcode)
+    
+    parametermode_one = 0
+
+    commands[commands[index+1]] = int(input("Input required: "))
+
+    return 0, num_arguments_needed
 
 
-def output(*args):
-    print("output ausgeführt", args)
-    return 0
+def output(opcode, index, commands):
+    num_arguments_needed = 1
+    parametermode_one, parametermode_two, parametermode_three = reveal_parameters_in_opcode(opcode)
+    if parametermode_one == 0:
+        print(commands[commands[index+1]])
+    
+    elif parametermode_one == 1:
+        print(commands[index+1])
+
+    return 0, num_arguments_needed
 
 
-def jump_if_true(*args):
-    print("jump_if_true ausgeführt", args)
-    return 0
+# - 5: jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+def jump_if_true(opcode, index, commands):
+    num_arguments_needed = 2
+    parametermode_one, parametermode_two, parametermode_three = reveal_parameters_in_opcode(opcode)
+
+    isTrue = False
+    if parametermode_one == 0:
+        if commands[commands[index+1]] != 0:
+            isTrue = True
+        
+    elif parametermode_one == 1:
+        if commands[index+1] != 0:
+            isTrue = True
+
+    if isTrue:
+        if parametermode_two == 0:
+            newpointer = int(commands[commands[index + 2]])
+        elif parametermode_two == 1:
+            newpointer = int(commands[index + 2])
+    elif isTrue == False:
+        newpointer = index + num_arguments_needed + 1
+
+    #return the newpointer because the runner sets the new pointer + 1, therefore the -1 is needed
+    return 0, newpointer - index - 1
 
 
-def jump_if_false(*args):
-    print("jump_if_false ausgeführt", args)
-    return 0
+def jump_if_false(opcode, index, commands):
+    num_arguments_needed = 2
+    parametermode_one, parametermode_two, parametermode_three = reveal_parameters_in_opcode(opcode)
+
+    isFalse = False
+    if parametermode_one == 0:
+        if commands[commands[index+1]] == 0:
+            isFalse = True
+        
+    elif parametermode_one == 1:
+        if commands[index+1] == 0:
+            isFalse = True 
+
+    if isFalse:
+        if parametermode_two == 0:
+            newpointer = int(commands[commands[index + 2]])
+        elif parametermode_two == 1:
+            newpointer = commands[index + 2]
+    elif isFalse == False:
+        newpointer = index + num_arguments_needed + 1
+
+    #return the newpointer because the runner sets the new pointer + 1, therefore the -1 is needed
+    return 0, newpointer - index - 1
 
 
-def less_than(*args):
-    print("less_than ausgeführt", args)
-    return 0
+def less_than(opcode, index, commands):
+    num_arguments_needed = 3
+    parametermode_one, parametermode_two, parametermode_three = reveal_parameters_in_opcode(opcode)
+
+    if parametermode_one == 0:
+        paramOne = int(commands[commands[index+1]])
+    elif parametermode_one == 1:
+        paramOne = commands[index+1]
+
+    if parametermode_two == 0:
+        paramTwo = int(commands[commands[index+2]])
+    elif parametermode_two == 1:
+        paramTwo = commands[index+2]
+        
+    paramThree = commands[index + 3]
+
+    if paramOne < paramTwo:
+        commands[paramThree] = 1
+    else:
+        commands[paramThree] = 0
+
+    return 0, num_arguments_needed
 
 
-def equals(*args):
-    print("equals ausgeführt", args)
-    return 0
+def equals(opcode, index, commands):
+    num_arguments_needed = 3
+    parametermode_one, parametermode_two, parametermode_three = reveal_parameters_in_opcode(opcode)
+
+    if parametermode_one == 0:
+        paramOne = int(commands[commands[index+1]])
+    elif parametermode_one == 1:
+        paramOne = commands[index+1]
+
+    if parametermode_two == 0:
+        paramTwo = int(commands[commands[index+2]])
+    elif parametermode_two == 1:
+        paramTwo = commands[index+2]
+        
+    paramThree = commands[index + 3]
+
+    if paramOne == paramTwo:
+        commands[paramThree] = 1
+    else:
+        commands[paramThree] = 0
+
+    return 0, num_arguments_needed
+
+def halt(opcode, index, commands):
+    global running
+    print("halt")
+    num_arguments_needed = 0
+    parametermode_one, parametermode_two, parametermode_three = reveal_parameters_in_opcode(opcode)
+
+    running = False 
+
+    return 0, num_arguments_needed
 
 
 opcode_dict = {
+    1 : addition,
+    2 : multiply,
     3 : save_at,
     4 : output,
     5 : jump_if_true,
     6 : jump_if_false,
     7 : less_than,
-    8 : equals
+    8 : equals,
+    99: halt
 }
 
 opcode_args_num = {
-    3 : 1,                  # save_at
-    4 : 1,                  # output
-    5 : 2,                  # jump_if_true
-    6 : 2,                  # jump_if_false
-    7 : 3,                  # less_than
-    8 : 2                   # equals
+    addition: 3,
+    multiply: 3,
+    save_at : 1,                  # save_at
+    output : 1,                  # output
+    jump_if_true : 2,                  # jump_if_true
+    jump_if_false : 2,                  # jump_if_false
+    less_than : 3,                  # less_than
+    equals : 3                   # equals
 }
 
+def read_opcode(opcodenumber):
+    correct_operation = halt
+    if opcodenumber <= 9:
+        try:
+            correct_operation = opcode_dict[opcodenumber]
+            #print(f"Der Opcode verweist auf folgende Funktion: {correct_operation}")
+        except KeyError:
+            print(f"Falscher Key wurde entdeckt mit der Abfrage des Opcodes in {opcodenumber}")
+    else:
+        opcode_operationkey = int(opcodenumber % 100)
+        correct_operation = opcode_dict[opcode_operationkey]
+    return correct_operation
 
+def runner(commands):
+    pointer = 0
+    while pointer < len(commands) and running == True:
 
+        correct_operation = read_opcode(commands[pointer])
+        
+        outputvalue, new_pointer = correct_operation(commands[pointer], pointer ,commands)
+        pointer += new_pointer + 1
+        #print(outputvalue, pointer)
 
-opcode_dict[3](1,2,3)
+#runner(commands)
+
+#testcommand = [1,2,2,3]
+#addition(1,0,testcommand)
+#print(testcommand)
+# Result is: [1, 2, 2, 4]
+
+runner(commands)
+#print(commands)
