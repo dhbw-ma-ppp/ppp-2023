afc95215878dcf67d13a05d75cfcfe2f07b69b06
thecodingcube77 <thecodingcube77@gmail.com>
2023-11-03 19:25:30 +0100

    Solved the first Task

---
+++ Exercises/BoWichtendahl/exercises_05.py
@@ -0,0 +1,80 @@
+# PART 1:
+# Here's a sequence of numbers:
+# [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576]
+# numbers in this list can in general be expressed as a sum of some pair of two numbers
+# in the five numbers preceding them.
+# For example, the sixth number (40) cam be expressed as 25 + 15
+# the seventh number (62) can be expressed as 47 + 15 etc.
+# 
+# The only exception to this rule for this example is the number 127.
+# The five preceding numbers are [95, 102, 117, 150, 182], and no possible sum of two of those
+# numbers adds to 127.
+#
+# You can find the ACTUAL input for this exercise under `data/input_sequence.txt`. For this
+# real input you should consider not only the 5 numbers, but the 25 numbers preceding.
+# Find the first number in this list which can not be expressed as a
+# sum of two numbers out of the 25 numbers before it.
+# Please make not of your result in the PR.
+
+import itertools
+import re
+
+
+def find_invalid_number(file_path, scan_length):
+    with open(file_path, 'r') as input_file:
+        nums = []
+        for num in range(scan_length):
+            nums.append(int(input_file.readline()))
+        replace_pointer = 0
+        while (in_str := input_file.readline()) != '':
+            num_to_check = int(in_str)
+            permutations = list(itertools.permutations(nums, 2))
+            if num_to_check not in [sum(perm) for perm in permutations]:
+                return num_to_check
+            nums[replace_pointer] = num_to_check
+            replace_pointer = (replace_pointer + 1) % scan_length
+
+
+print(f'The first invalid number in the given file is: {find_invalid_number('../../data/input_sequence.txt', 25)}')
+
+# PART 2:
+# The input to this exercise specifies rules for bags containing other bags.
+# It is of the following form:
+#
+# shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+# dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+# vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+# faded blue bags contain no other bags.
+# dotted black bags contain no other bags.
+#
+# You have a single 'shiny gold bag'. Consider the rules above. According
+# to those rules your bag contains
+# - 1 dark olive bag, in turn containing
+#   - 3 faded blue bags (no further content)
+#   - 4 dotted black bags (no further content
+# - 2 vibrant plum bags, in turn containing
+#   - 5 faded blue bags (no further content)
+#   - 6 dotted black bags (no further content)
+# 
+# therefore, your single shiny gold bag contains a total of 32 bags
+# (1 dark olive bag, containing 7 other bags, and 2 vibrant plum bags,
+# each of which contains 11 bags, so 1 + 1*7 + 2 + 2*11 = 32)
+#
+# The ACTUAL input to your puzzle is given in `data/input_bags.txt`, and much larger
+# and much more deeply nested than the example above. 
+# For the actual inputs, how many bags are inside your single shiny gold bag?
+# As usual, please list the answer as part of the PR.
+
+
+# def count_bags(file_path):
+#    bag_dict = {}
+#    with open(file_path, 'r') as input_file:
+#        while (input_line := input_file.readline()) != '':
+#            input_line = input_line.split(' contain ')
+#            print(input_line[1].split(', '))
+
+
+#count_bags('../../data/input_bags.txt')
+
+s_nums = 'drab white bags contain 1 drab lavender bag, 1 plaid maroon bag.'
+print(re.split(r" bags\.| bag\.| bags, | bag, | bags contain ", s_nums))
