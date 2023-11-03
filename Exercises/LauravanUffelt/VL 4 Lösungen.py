13404e14faa52f80a232f7253ad0296df0277d45
Laura van Uffelt <laura.vanuffelt@icloud.com>
2023-11-03 09:02:21 +0100

    changes

--- Exercises/LauravanUffelt/VL 4 Lösungen.py
+++ Exercises/LauravanUffelt/VL 4 Lösungen.py
@@ -50,19 +50,20 @@ def comp_sim(intake):
         opcode = int(operation[-2:])
         mode = [int(digit) for digit in operation[:-2]][::-1]
         element1 = element_value(mode[0], intake[count+1])
-        element2 = element_value(mode[1], intake[count+2])
 
         match opcode:
             case 99:
-                return None
+                exit(0)
 
             case 1:
+                element2 = element_value(mode[1], intake[count+2])
                 sum = element1 + element2
                 adress = intake[count+3]
                 intake[adress] = sum
                 return count + 4
 
             case 2:
+                element2 = element_value(mode[1], intake[count+2])
                 product = element1 * element2
                 adress = intake[count+3]
                 intake[adress] = product
@@ -80,6 +81,7 @@ def comp_sim(intake):
                 return count + 2
 
             case 5:  # jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+                element2 = element_value(mode[1], intake[count+2])
                 if element1 != 0:
                     count = element2
                     return count
@@ -87,6 +89,7 @@ def comp_sim(intake):
                     return count + 3
 
             case 6:  # jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+                element2 = element_value(mode[1], intake[count+2])
                 if element1 == 0:
                     count = element2
                     return count
@@ -94,6 +97,7 @@ def comp_sim(intake):
                     return count + 3
 
             case 7:  # less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+                element2 = element_value(mode[1], intake[count+2])
                 adress = intake[count+3]
                 if element1 < element2:
 
@@ -103,6 +107,7 @@ def comp_sim(intake):
                     intake[adress] = 0
                     return count + 4
             case 8:  # equals: if the first parameters equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+                element2 = element_value(mode[1], intake[count+2])
                 adress = intake[count+3]
                 if element1 == element2:
                     intake[adress] = 1
@@ -114,13 +119,8 @@ def comp_sim(intake):
                 print("ERROR")
 
     count = 0
-    while count < len(intake)-2:
-        result = opcodes(count)
-        if isinstance(result, int):
-            count = result
-        else:
-            pass
-    return result
+    while count < len(intake):
+        count = opcodes(count)
 
 
 commands = [3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1101, 40, 71, 224, 1001, 224, -111, 224, 4, 224, 1002, 223, 8, 223, 101, 7, 224, 224, 1, 224, 223, 223, 1102, 66, 6, 225, 1102, 22, 54, 225, 1, 65, 35, 224, 1001, 224, -86, 224, 4, 224, 102, 8, 223, 223, 101, 6, 224, 224, 1, 224, 223, 223, 1102, 20, 80, 225, 101, 92, 148, 224, 101, -162, 224, 224, 4,224,1002,223,8,223,101,5,224,224,1,224,223,223,1102,63,60,225,1101,32,48,225,2,173,95,224,1001,224,-448,224,4,224,102,8,223,223,1001,224,4,224,1,224,223,223,1001,91,16,224,101,-79,224,224,4,224,1002,223,8,223,101,3,224,224,1,224,223,223,1101,13,29,225,1101,71,70,225,1002,39,56,224,1001,224,-1232,224,4,224,102,8,223,223,101,4,224,224,1,223,224,223,1101,14,59,225,102,38,143,224,1001,224,-494,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1102,30,28,224,1001,224,-840,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,107,677,226,224,1002,223,2,223,1005,224,329,1001,223,1,223,8,226,226,224,102,2,223,223,1006,224,344,101,1,223,223,7,226,677,224,1002,223,2,223,1005,224,359,101,1,223,223,1007,677,226,224,1002,223,2,223,1005,224,374,1001,223,1,223,1007,677,677,224,1002,223,2,223,1006,224,389,101,1,223,223,1008,226,226,224,1002,223,2,223,1005,224,404,1001,223,1,223,108,677,226,224,1002,223,2,223,1006,224,419,1001,223,1,223,1108,677,226,224,102,2,223,223,1006,224,434,1001,223,1,223,108,226,226,224,1002,223,2,223,1005,224,449,101,1,223,223,7,677,677,224,1002,223,2,223,1006,224,464,1001,223,1,223,8,226,677,224,1002,223,2,223,1005,224,479,1001,223,1,223,107,226,226,224,102,2,223,223,1006,224,494,101,1,223,223,1007,226,226,224,1002,223,2,223,1005,224,509,1001,223,1,223,1107,226,677,224,102,2,223,223,1005,224,524,1001,223,1,223,108,677,677,224,1002,223,2,223,1005,224,539,101,1,223,223,1107,677,226,224,102,2,223,223,1005,224,554,1001,223,1,223,107,677,677,224,1002,223,2,223,1005,224,569,101,1,223,223,8,677,226,224,102,2,223,223,1005,224,584,1001,223,1,223,7,677,226,224,102,2,223,223,1006,224,599,101,1,223,223,1008,677,677,224,1002,223,2,223,1005,224,614,101,1,223,223,1008,677,226,224,102,2,223,223,1006,224,629,1001,223,1,223,1108,677,677,224,102,2,223,223,1006,224,644,101,1,223,223,1108,226,677,224,1002,223,2,223,1005,224,659,1001,223,1,223,1107,226,226,224,102,2,223,223,1006,224,674,1001,223,1,223,4,223,99,226]
