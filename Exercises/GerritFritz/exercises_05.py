727a46917d1463831360b76bd4f4da12d9e60697
GeFritz <gfritz.business@gmail.com>
2023-11-03 20:53:06 +0100

    some stylistic changes

--- Exercises/GerritFritz/exercises_05.py
+++ Exercises/GerritFritz/exercises_05.py
@@ -3,8 +3,6 @@
 
 import pathlib
 
-numbers1 = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576]
-
 def find_num(numbers, dist):
     for i in range(dist,len(numbers)):
         previous = numbers[(i-dist):i]
@@ -18,26 +16,21 @@ def find_num(numbers, dist):
 
 seq_directory = str(pathlib.Path(__file__).parent.resolve().parent.parent)+"\data\input_sequence.txt"
 
-numbers2 = []
+numbers = []
 with open(seq_directory) as file:
     for line in file:
-        numbers2.append(int(line[:-1]))
+        numbers.append(int(line[:-1]))
 
-print(find_num(numbers2, 25))
+print(find_num(numbers, 25))
 
 
 class Node():
     def __init__(self, parent, name):
         self.name = name
         self.nodenum = 0
-        self.subnodes = []
         self.parent = parent
         if self.parent != None:
-            self.parent.add_node(self)
             self.parent.increase()
-
-    def add_node(self, node):
-        self.subnodes.append(node)
     
     def increase(self):
         self.nodenum += 1
@@ -46,9 +39,6 @@ class Node():
     
     def get_count(self):
         return self.nodenum
-    
-    def __str__(self):
-        return f"{self.name}: \n({', '.join([str(node)for node in self.subnodes])})\n"
 
 
 def insert_to_tree(bag_dict, parent, proot):
@@ -67,8 +57,8 @@ with open(bag_directory) as file:
     for line in file:
         spliced = line.split("bags contain")
         parent = spliced[0]
-        kid = [chars.split(" ")[(1 if i == 0 else 0):-1] for i, chars in enumerate(spliced[1].split(", "))]
-        bag_dict[parent] = kid         
+        child = [chars.split(" ")[(1 if i == 0 else 0):-1] for i, chars in enumerate(spliced[1].split(", "))]
+        bag_dict[parent] = child
 
 
 root = Node(None, "shiny gold ")
