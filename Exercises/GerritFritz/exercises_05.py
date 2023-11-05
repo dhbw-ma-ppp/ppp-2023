6019c282d95d5f932033008e057fa53093136544
GeFritz <gfritz.business@gmail.com>
2023-11-05 13:48:41 +0100

    made code faster

--- Exercises/GerritFritz/exercises_05.py
+++ Exercises/GerritFritz/exercises_05.py
@@ -14,43 +14,26 @@ def find_num(numbers, dist):
         if not is_sum:
             return numbers[i]
 
-seq_directory = str(pathlib.Path(__file__).parent.resolve().parent.parent)+"\data\input_sequence.txt"
+seq_directory = str(pathlib.Path(__file__).parent.resolve().parent.parent)+"/data/input_sequence.txt"
 
 numbers = []
 with open(seq_directory) as file:
     for line in file:
         numbers.append(int(line[:-1]))
-
 print(find_num(numbers, 25))
 
 
-class Node():
-    def __init__(self, parent):
-        self.nodenum = 0
-        self.parent = parent
-        if self.parent != None:
-            self.parent.increase()
-    
-    def increase(self):
-        self.nodenum += 1
-        if self.parent != None:
-            self.parent.increase()
-    
-    def get_count(self):
-        return self.nodenum
-
-
-def insert_to_tree(bag_dict, parent, parent_node):
+def count_bags(bag_dict, parent):
     bags = bag_dict[parent]
     for bag in bags:
         if len(bag)<3: continue
         name = f"{bag[1]} {bag[2]} "
-        for node in [Node(parent_node) for n in range(int(bag[0]))]:
-            insert_to_tree(bag_dict, name, node)
-
-
-bag_directory = str(pathlib.Path(__file__).parent.resolve().parent.parent)+"\data\input_bags.txt"
-
+        for sub_bags in range(int(bag[0])):
+            global bag_counter
+            bag_counter += 1
+            count_bags(bag_dict, name)
+    
+bag_directory = str(pathlib.Path(__file__).parent.resolve().parent.parent)+"/data/input_bags.txt"
 bag_dict = {}
 with open(bag_directory) as file:
     for line in file:
@@ -60,7 +43,6 @@ with open(bag_directory) as file:
         children = [chars.split(" ")[(not i):-1] for i, chars in enumerate(arguments)]
         bag_dict[parent] = children
 
-
-root = Node(None)
-insert_to_tree(bag_dict, "shiny gold ", root)
-print(root.get_count())
+bag_counter = 0
+count_bags(bag_dict, "shiny gold ")
+print(bag_counter)
