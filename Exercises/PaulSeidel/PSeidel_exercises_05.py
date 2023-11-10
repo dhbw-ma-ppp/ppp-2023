e99e09e36a8d170f80d08ee05b1b2bebe692fc23
paulseidel08 <s231363@student.dhbw-mannheim.de>
2023-11-10 09:51:39 +0100

    finished part 2 now

--- Exercises/PaulSeidel/PSeidel_exercises_05.py
+++ Exercises/PaulSeidel/PSeidel_exercises_05.py
@@ -38,17 +38,14 @@ def check_if_sum(sequence):
             missing_rest = sequence[counter]-value1         #the rest of the subtraction is the missing part of the combination
             #if value1_index < last_index:
             for value2_index in range(value1_index+1,last_index+1):
-                value2 = sequence[value2_index]         #index of the 2nd value for addition
-                if value2 == missing_rest:              #checking if the value2 equals to the missing rest
+                value2 = sequence[value2_index]             #index of the 2nd value for addition
+                if value2 == missing_rest:                  #checking if the value2 equals to the missing rest
                     found_smth += 1
         if found_smth == 0:
             return (f"the integer at sequence[{counter}] = {sequence[counter]} can't be build out of the 25 Integers in front of it!")
             #value1_index += 1
 
-#print(check_if_sum(sequence))
-#print(sequence)
-
-
+print(check_if_sum(sequence))
 
 # PART 2:
 # The input to this exercise specifies rules for bags containing other bags.
@@ -79,79 +76,41 @@ def check_if_sum(sequence):
 # As usual, please list the answer as part of the PR.
 
 with open(".\data\input_bags.txt", "r") as file_bags:
-    list_of_lines = [a.rstrip("\n") for a in file_bags.readlines()] #method rstrip("\n") removes every \n from the end of every String
+    list_of_lines = [a.rstrip(".\n") for a in file_bags.readlines()]
 
-amount_of_sgb = 0   #sgb= shiny gold bag
-set_of_bags = set()
 dict_of_bags = dict()
+dict_of_values = dict()
 
-#print(list_of_lines)
+def dictAllBags():
+    """edits the dictionary by writing the names of the bags as string-key and the description as string-value"""
 
-def listAllBags():
-    """takes the global list of lines that describe the bags as argument and lists every bag once in a global set"""
-
-    for i in range(len(list_of_lines)):
-        element_raw = list_of_lines[i]
+    for element_raw in list_of_lines:
         element_splitted = element_raw.split(" bags contain ")
-        set_of_bags.add(element_splitted[0])
-    #return set_of_bags
+        dict_of_bags.update({(f"{element_splitted[0]}"): element_splitted[1]})
+        dict_of_values.update({(f"{element_splitted[0]}"): 0})
 
-def createDict():
-    """creates a global dictionary out of the global set of bags. The keys are the name of each bag and the values all equal 0. Those will later be
-    counters for the amount of similar bags"""
 
-    set_of_bags_as_list = list(set_of_bags)
-    for i in range(len(set_of_bags_as_list)):
-        element = set_of_bags_as_list[i]
-        dict_of_bags.update({(f"{element}"): 0})
+def recursive_counter(key):
+    """adds the amount of inner bags out of one line of the list_of_lines and returns the amount of bags in the current bag (key)"""
+    
+    description = str(dict_of_bags.get(f"{key}"))
+    description = description.split(", ")
 
-def countIntoDict():
-    """goes through the global list of bag-descriptions and counts the amount of bags of every kind in the global dictionary already containing all bags once as keys. 
-    at the end the global dictionary contains all bags as keys and their amount"""
+    if description[0] != ("no other bags"): #recursion's break
+        return_value = 0
+        for part in description:
+            part = part.split()
+            next_bag = f"{part[1]} {part[2]}"
+            content = recursive_counter(next_bag)
+            amount = int(part[0])
+            dict_of_values.update({f"{key}": f"{amount}"})
+            return_value += amount + amount * content
+        return return_value
+            
+    else:
+        return 0
 
-    for element in list_of_lines:
-        splitted_whole_element = element.split(" bags contain ")            #splits the element to seperate the bag from it's content (inner bags)
-        splitted_content = splitted_whole_element[1].split(", ")            #splits the inner bags
-        for bag_name in splitted_content:
-            if bag_name != ("no other bags."):
-                splitted_instruction = bag_name.split()                     #splits the String (e.g. 5 faded blue bags) into words
-                amount = splitted_instruction[0]                       #amount of bags of one kind
-                amount = int(amount)
-                key = f"{splitted_instruction[1]} {splitted_instruction[2]}"#name of the bag to be used as key in dictionary
-                value = dict_of_bags.get(f"{key}")                          #amound of bags added to the current value in the dict
-                value += amount
-                dict_of_bags.update({f"{key}": value})                      #update the dict with the new value
 
-def evaluateDict():
-    """adds the total amount of all bags (without shiny gold bags) into one counter and divides it by the amount of shiny gold bags. The result is than the amount of bags
-    in one shiny gold bag"""
+dictAllBags()
 
-    bag_counter = 0
-    list_of_keys = dict_of_bags.keys()
-    for key in list_of_keys:
-        if key != "shiny gold":                         #to exclude all shiny bags.
-            bag_counter += int(dict_of_bags.get(f"{key}"))
-
-    amount_of_sgb = dict_of_bags.get("shiny gold")      #sgb= shiny gold bag
-    bag_counter //= amount_of_sgb
-
-    return int(bag_counter)
-
-def mainFunction():     #not necessary in Python, but easier to understand later on
-
-    listAllBags()
-    createDict()
-    countIntoDict()
-    bags_in_gold_bag = evaluateDict()
-    gold_bags = dict_of_bags.get("shiny gold")
-    bag_counter = 0
-
-    list_of_keys = dict_of_bags.keys()
-    for key in list_of_keys:
-        bag_counter += int(dict_of_bags.get(f"{key}"))
-
-    print(f"Every shiny gold bag contains {bags_in_gold_bag} other bags.")
-    print(f"Info: There are {gold_bags} shiny gold bags in total.")
-    print(f"There are {bag_counter} bags in total")
-
-mainFunction()
+print(recursive_counter("shiny gold")) 
