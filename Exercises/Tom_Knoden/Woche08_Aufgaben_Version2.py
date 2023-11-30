bda97bb02d238118ea0e96f4b1ba73e00bc91ea4
TomKnoden <146974535+TomKnoden@users.noreply.github.com>
2023-11-30 22:55:26 +0100

    Update Woche08_Aufgaben_Version2.py
    I've adressed the change requests made by @TrueEsel

--- Exercises/Tom_Knoden/Woche08_Aufgaben_Version2.py
+++ Exercises/Tom_Knoden/Woche08_Aufgaben_Version2.py
@@ -1,72 +1,88 @@
-# Prepare an analysis of the titanic dataset (data/titanic.csv).
-# You are interested in determining factors that lead to death or survival of passengers.
-# However, before actually training an ML model to predict death or survival, you should make
-# yourself familiar with the data.
-#
-# Look into which features are available, what types of features these are, whether
-# values are missing and how to deal with them, etc.
-# You can then continue to create some visualizations of things you think might be interesting or relevant
-# in predicting survival, or just generally useful to know about the dataset.
-# The output should not just be prints of numbers or figures, but also some explanatory text of
-# what you have been analysing, and which conclusions you can draw from the different steps of your analysis.
-#
-# You can do all of this in a Jupyter Notebook, embedding analysis, code and explanations in a single document.
-# Alternatively you can create a separate script for the analysis, and then list results, figures and explanations
-# either in the PR directly, or in some form of pdf/word/markdown/... document.
-#
-# For this exercise I very much encourage you to work together to come up with ideas of which
-# calculations/plots/... might be interesting to analyse. You will find very many such analyses online
-# for the titanic data, but I would recommend trying to think of your own ideas first, and only later
-# look for more inspiration online.
-
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-input_file_path = Path('data/titanic.csv')
+root_dir = Path(__file__).absolute().parents[2]
+input_file_path = root_dir / "data" / "titanic.csv"
 titanic_data = pd.read_csv(input_file_path)
 
-#all the basic information
+
+#Basic Information
+print("Basic Information:")
+print("The dataset contains information about passengers on the Titanic.")
+print("It includes details such as age, sex, passenger class, and survival status.")
 print(titanic_data.info())
+print("\n")
 
-#summary statistics for numerical columns
+#Summary Statistics for Numerical Columns
+print("Summary Statistics for Numerical Columns:")
+print("Here are the summary statistics for numerical columns in the dataset.")
 print(titanic_data.describe())
+print("\n")
 
-#Checking for missing values
+#Missing Values
+print("Missing Values:")
+print("Checking for missing values in the dataset.")
 print(titanic_data.isnull().sum())
+print("\n")
 
-#survival rate
+#Survival Rate
+print("Survival Rate:")
+print("Analyzing the overall survival rate of passengers.")
 survival_count = titanic_data['Survived'].value_counts()
-survival_count.plot(kind='bar', color=['skyblue', 'salmon'])
-plt.title('Survival Count')
+survival_percentage = survival_count / len(titanic_data) * 100
+print(survival_percentage)
+survival_percentage.plot(kind='bar', color=['skyblue', 'salmon'])
+plt.title('Survival Percentage')
 plt.xticks(rotation=0)
 plt.show()
+print("\n")
 
-#survival rate by Pclass
+#Survival Rate by Pclass
+print("Survival Rate by Pclass:")
+print("Analyzing the survival rate based on passenger class (Pclass).")
 pclass_survival = titanic_data.groupby(['Pclass', 'Survived']).size().unstack()
-pclass_survival.plot(kind='bar', stacked=True, color=['skyblue', 'salmon'])
-plt.title('Survival Count by Pclass')
+pclass_survival_percentage = pclass_survival.div(pclass_survival.sum(axis=1), axis=0) * 100
+print(pclass_survival_percentage)
+pclass_survival_percentage.plot(kind='bar', stacked=True, color=['skyblue', 'salmon'])
+plt.title('Survival Percentage by Pclass')
 plt.xticks(rotation=0)
 plt.show()
+print("\n")
 
-#survival rate by Sex
+#Survival Rate by Sex
+print("Survival Rate by Sex:")
+print("Analyzing the survival rate based on gender (Sex).")
 sex_survival = titanic_data.groupby(['Sex', 'Survived']).size().unstack()
-sex_survival.plot(kind='bar', stacked=True, color=['skyblue', 'salmon'])
-plt.title('Survival Count by Sex')
+sex_survival_percentage = sex_survival.div(sex_survival.sum(axis=1), axis=0) * 100
+print(sex_survival_percentage)
+sex_survival_percentage.plot(kind='bar', stacked=True, color=['skyblue', 'salmon'])
+plt.title('Survival Percentage by Sex')
 plt.xticks(rotation=0)
 plt.show()
+print("\n")
 
-#survival rate by Age group
+#Survival Rate by Age group
+print("Survival Rate by Age Group:")
+print("Analyzing the survival rate based on age groups.")
 titanic_data['AgeGroup'] = pd.cut(titanic_data['Age'], bins=[0, 18, 35, 50, 80], labels=['0-18', '19-35', '36-50', '51-80'])
 age_group_survival = titanic_data.groupby(['AgeGroup', 'Survived']).size().unstack()
-age_group_survival.plot(kind='bar', stacked=True, color=['skyblue', 'salmon'])
-plt.title('Survival Count by Age Group')
+age_group_survival_percentage = age_group_survival.div(age_group_survival.sum(axis=1), axis=0) * 100
+print(age_group_survival_percentage)
+age_group_survival_percentage.plot(kind='bar', stacked=True, color=['skyblue', 'salmon'])
+plt.title('Survival Percentage by Age Group')
 plt.xticks(rotation=0)
 plt.show()
+print("\n")
 
-#survival rate by Embarked
+#Survival Rate by Embarked
+print("Survival Rate by Embarked:")
+print("Analyzing the survival rate based on the port of embarkation (Embarked).")
 embarked_survival = titanic_data.groupby(['Embarked', 'Survived']).size().unstack()
-embarked_survival.plot(kind='bar', stacked=True, color=['skyblue', 'salmon'])
-plt.title('Survival Count by Embarked')
+embarked_survival_percentage = embarked_survival.div(embarked_survival.sum(axis=1), axis=0) * 100
+print(embarked_survival_percentage)
+embarked_survival_percentage.plot(kind='bar', stacked=True, color=['skyblue', 'salmon'])
+plt.title('Survival Percentage by Embarked')
 plt.xticks(rotation=0)
 plt.show()
+print("\n")
